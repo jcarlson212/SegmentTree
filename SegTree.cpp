233173380e135a3d7347e7a304ac624b4dcc9ecb
jcarlson212 <44142882+jcarlson212@users.noreply.github.com>
2020-12-13 09:52:06 -0700

    Removed imports and templated

--- SegTree.cpp
+++ SegTree.cpp
@@ -34,12 +34,13 @@ typedef unsigned long long int  uint64;
 
 using namespace std;
 
+template <class T>
 class SegTree {
     private:
-        vector<int> input_array;
-        vector<int> seg_tree;
+        vector<T> input_array;
+        vector<T> seg_tree;
 
-        int query_range_helper(int left_index, int right_index, int seg_index, int current_left_index, int current_right_index){
+        T query_range_helper(T left_index, T right_index, T seg_index, T current_left_index, T current_right_index){
             if(right_index < current_left_index || left_index > current_right_index){
                 //not covered
                 return 0;
@@ -53,7 +54,7 @@ class SegTree {
             }
         }
 
-        void insert_helper(int index, int value, int seg_index, int left_index, int right_index){
+        void insert_helper(T index, T value, T seg_index, T left_index, T right_index){
             if(index >= left_index && index <= right_index){
                 seg_tree[seg_index] += value;
                 if(left_index == right_index){
@@ -66,44 +67,44 @@ class SegTree {
             }
         }
 
-        int get_left_child_index(int index){
+        T get_left_child_index(T index){
             return 2*index + 1;
         }
 
-        int get_right_child_index(int index){
+        T get_right_child_index(T index){
             return 2*index + 2;
         }
 
     public:
-        SegTree(vector<int> input_array){
+        SegTree(vector<T> input_array){
             this->input_array = input_array;
-            seg_tree = vector<int>(2*input_array.size(), 0);
+            seg_tree = vector<T>(4*input_array.size()+1, 0);
             build_seg_tree(0, 0, this->input_array.size()-1);
         }
 
         //O(2*n) time complexity (n + n/2 + ... = 2*n). O(log(n)) space complexity for call stack. This could be reduced to O(1) by making it iterative 
-        int build_seg_tree(int seg_index, int left_index, int right_index){
+        T build_seg_tree(T seg_index, T left_index, T right_index){
             if(right_index == left_index){
                 this->seg_tree[seg_index] = this->input_array[left_index];
             }else{
-                int left_sum = build_seg_tree(get_left_child_index(seg_index), left_index, (left_index + right_index)/2);
-                int right_sum = build_seg_tree(get_right_child_index(seg_index), (left_index + right_index)/2 + 1, right_index);
+                T left_sum = build_seg_tree(get_left_child_index(seg_index), left_index, (left_index + right_index)/2);
+                T right_sum = build_seg_tree(get_right_child_index(seg_index), (left_index + right_index)/2 + 1, right_index);
                 this->seg_tree[seg_index] = left_sum + right_sum;
             }
             return this->seg_tree[seg_index];
         }
 
         //O(log(n)) time-complexity. O(log(n)) space-complexity from stack. Can be reduced to O(1) space.
-        void insert(int index, int value){
+        void insert(T index, T value){
             insert_helper(index, (value - this->input_array[index]), 0, 0, this->input_array.size()-1);
         }
 
         //assumes a valid query input (seg_index = 0 )
-        int query_range_sum(int left_index, int right_index){
+        T query_range_sum(T left_index, T right_index){
             return query_range_helper(left_index, right_index, 0, 0, this->input_array.size()-1);
         }
 
-        void print_seg_tree(int seg_index, int left_index, int right_index){
+        void print_seg_tree(T seg_index, T left_index, T right_index){
             if(seg_index >= this->seg_tree.size()){
                 return;
             }
@@ -114,11 +115,11 @@ class SegTree {
             }
         }
 
-        int get_arr_size(){
+        T get_arr_size(){
             return this->input_array.size();
         }
 
-        int get_tree_size(){
+        T get_tree_size(){
             return this->seg_tree.size();
         }
 
@@ -128,7 +129,7 @@ int main(){
     ios::sync_with_stdio(0); 
     cin.tie(0); 
 
-    SegTree t({1,2,3,4});
+    SegTree<int> t({1,2,3,4});
 
     t.print_seg_tree(0, 0, 3);
     t.insert(0, 4);
